5b75636ff4f73891e10ad6022071e0b60d44afd7
David de Jesús Martínez Mayeta <david.martinez@aoshield.com>
2025-04-12 10:10:23 -0400

    feat: add OutletContainer component for wrapping children with styled Box

---
+++ src/components/OutletContainer.tsx
@@ -0,0 +1,23 @@
+import { Box } from '@mui/material';
+
+/**
+ * A container component that wraps its children with a styled Box.
+ * It provides padding and ensures the content takes the full width.
+ *
+ * @param props - The props object containing children.
+ * @param props.children - The content to be rendered inside the container.
+ * @returns  The rendered container component.
+ */
+export const OutletContainer: React.FC = ({ children }) => {
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        pt: 10,
+        width: '100%',
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
